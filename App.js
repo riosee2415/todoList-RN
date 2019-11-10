90ff77d590cc365d056e5f84d63ee62a32ce1a3c
DEV_YUN <riosee2415@gmail.com>
2019-11-10 23:27:49 +0900

    UI screen

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <LinearGradient colors={["#E4E5E6", "#00416A"]} style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.title}>RN To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To Do.."} />
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#dff9fb",
+    alignItems: "center"
   },
+  title: {
+    color: "#ffffff",
+    fontSize: 30,
+    marginTop: 80,
+    fontWeight: "600",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "#ffffff",
+    flex: 1,
+    width: width - 30,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
 });
